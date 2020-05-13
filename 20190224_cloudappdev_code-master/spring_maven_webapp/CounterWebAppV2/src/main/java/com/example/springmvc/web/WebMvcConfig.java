8134f823ab7b2ce8c8539b4b27939cfc99cf99ef
XuYongjia17 <63069171+XuYongjia17@users.noreply.github.com>
2020-05-13 19:17:43 +0800

    Add files via upload

---
+++ 20190224_cloudappdev_code-master/spring_maven_webapp/CounterWebAppV2/src/main/java/com/example/springmvc/web/WebMvcConfig.java
@@ -0,0 +1,32 @@
+package com.example.springmvc.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * Web application configuration.
+ *
+ * @author bobyuan
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    /**
+     * locate our static resources and serve them.
+     */
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**").addResourceLocations("/assets/");
+    }
+
+    @Controller
+    static class FaviconController {
+        @GetMapping("favicon.ico")
+        public String favicon() {
+            return "forward:/assets/img/favicon.ico.png";
+        }
+    }
+}
